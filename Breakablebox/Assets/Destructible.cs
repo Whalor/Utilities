79e33cb95baf6abb04cd04a243794128ab8591cc
Whalor <54701426+Whalor@users.noreply.github.com>
2020-09-26 04:52:40 -0600

    Breakable Box
    Made a breakable box script that applies physics.

---
+++ Breakablebox/Assets/Destructible.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour
+{
+    public GameObject destroyedVersion;
+    Rigidbody rb_cube;
+    private bool left = false;
+    private bool right = false;
+
+    private void OnMouseDown()
+    {
+        Instantiate(destroyedVersion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        left = Input.GetKeyDown("a");
+        right = Input.GetButtonDown("D");
+    }
+
+    private void FixedUpdate()
+    {
+        if (left)
+        {
+            Instantiate(destroyedVersion, transform.position, transform.rotation);
+            Destroy(gameObject);
+            for (int i=0; i<destroyedVersion.transform.childCount; i++)
+            {
+                GameObject child = destroyedVersion.transform.GetChild(i).gameObject;
+                Rigidbody childCube = child.GetComponent<Rigidbody>();
+                childCube.velocity = new Vector3(0, 0, 10);
+
+            }
+        }
+    }
+}
